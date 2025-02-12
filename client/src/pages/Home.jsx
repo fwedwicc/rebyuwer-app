c0a0006975ce30607272ada9c31d028ebee1240a
fwedwicc <moreno.frederick.capiral@gmail.com>
2025-02-12 10:59:50 +0800

    fetch and display card sets and their associated cards on the home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,8 @@ import api from '../utils/api'
 
 const Home = () => {
   const [user, setUser] = useState(null)
+  const [cardSets, setCardSets] = useState([])
+  const [cards, setCards] = useState({})
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState('')
   const [formData, setFormData] = useState({
@@ -51,6 +53,46 @@ const Home = () => {
     }
   }
 
+  // Fetch Card Set
+  useEffect(() => {
+    const fetchCardSets = async () => {
+      try {
+        const response = await api.get('/cardSet')
+        setCardSets(response.data)
+      } catch (error) {
+        console.log('Error fetching cards:', error)
+      }
+    }
+
+    fetchCardSets()
+    const interval = setInterval(fetchCardSets, 5000)
+    return () => clearInterval(interval)
+  }, [])
+
+  // Fetch Cards for Each Card Set
+  useEffect(() => {
+    const fetchCards = async () => {
+      try {
+        const fetchedCards = {}
+
+        await Promise.all(
+          cardSets.map(async (set) => {
+            const response = await api.get(`/card/${set._id}`)
+            fetchedCards[set._id] = response.data.data // Store cards by cardSetId
+          })
+        )
+
+        setCards(fetchedCards) // Update state with fetched cards
+      } catch (error) {
+        console.error('Error fetching cards:', error.response || error)
+      }
+    }
+
+    if (cardSets.length > 0) {
+      fetchCards()
+    }
+  }, [cardSets])
+
   return (
     <div>
       <h1>Home Peyds</h1>
@@ -73,7 +115,30 @@ const Home = () => {
         </button>
       </form>
       {/* Card Sets */}
-      <div className='border border-green-500 p-4'></div>
+      <div className='border border-green-500 p-4'>
+        {Array.isArray(cardSets) && cardSets.length > 0 ? (
+          cardSets.map((set) => (
+            <li key={set._id}>
+              <p>Name: {set.name}</p>
+              {/* Cards */}
+              {Array.isArray(cards[set._id]) && cards[set._id].length > 0 ? (
+                <ul>
+                  {cards[set._id].map((card) => (
+                    <li key={card._id}>
+                      <p>{card.question}</p>
+                      <p>{card.answer}</p>
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <p>No cards available</p>
+              )}
+            </li>
+          ))
+        ) : (
+          <li>No card set available</li>
+        )}
+      </div>
     </div>
   )
 }
